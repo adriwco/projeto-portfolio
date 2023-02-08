4281a8d9e422f3c9e904a79074a8ccd7e6e81e80
adriwco <adriwco@gmail.com>
2023-02-08 15:51:19 -0300

    :dizzy: effects [section] : initAnimacaoScroll()

--- js/script.js
+++ js/script.js
@@ -2,3 +2,24 @@
   if(window.SimpleAnime){
     new SimpleAnime();
   }
+
+  function initAnimacaoScroll() {
+    const sections = document.querySelectorAll(".js-scroll");
+  
+    if (sections.length) {
+      const windowMetade = window.innerHeight * 0.6;
+  
+      function animaScroll() {
+        sections.forEach((section) => {
+          const sectionTop = section.getBoundingClientRect().top;
+          const isSectionVisible = sectionTop - windowMetade < 0;
+          if (isSectionVisible) section.classList.add("ativo");
+          else section.classList.remove("ativo");
+        });
+      }
+      animaScroll(); // exibir a 1° section
+  
+      window.addEventListener("scroll", animaScroll);
+    }
+  }
+  initAnimacaoScroll();
